0ace39003d4567659edc50ed1340d71d52a14a8e
aroy2003 <alden.j.roy@gmail.com>
2023-10-31 11:30:43 -0700

    updated command line project functionality.

--- Sprint1/project_part1.py
+++ Sprint1/project_part1.py
@@ -6,16 +6,43 @@ print("Enter 3 to convert temperature")
 user_input = input("Enter your choice: ")
 
 while user_input != "4":
+    if user_input == "h":
+        print("Welcome to the CS361 weather app")
+        print("You can enter 4 to exit, b to go back one, h to come back to the home page.")
+        print("Enter 1 to continue")
+        print("Enter 2 to enter your city")
+        print("Enter 3 to convert temperature")
+        user_input = input("Enter your choice: ")
+        
     if user_input == "1":
         country = input("Enter your country: ")
         #follow order in your doc
+        if country == "h":
+            user_input = "h"
+        elif country == "b":
+            user_input = "h"
+            #send home or back by input
+        else:
+            user_input = "2"
 
     elif user_input == "2":
         city = input("Enter your city: ")
+        if city == "h":
+            user_input = "h"
+        elif city == "b":
+            user_input = "1"
+            #send home or back by input
+        else:
+            user_input = "3"
 
     elif user_input == "3":
         choice = input("Enter 1 to convert from C to F 2 to convert F to C: ")
-        if choice == "1":
+        if choice == "h":
+            user_input = "h"
+        elif choice == "b":
+            user_input = "2"
+            #send home or back by input
+        elif choice == "1":
             temp = input("Enter your temp in Celsius: ")
             if temp[0].isdigit():
                 temp = int(temp)
@@ -25,7 +52,7 @@ while user_input != "4":
                 print("Please enter a valid temperature.")
 
 
-        if choice == "2":
+        elif choice == "2":
             temp = input("Enter your temp in Fahrenheit: ")
             if temp[0].isdigit():
                 temp = int(temp)
